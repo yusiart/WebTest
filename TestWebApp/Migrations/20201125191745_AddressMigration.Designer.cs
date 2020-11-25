476e36aa5fcd95e9bc82ac43319bb9d0583ce71c
Yura Vishnyakov <yuravishnyakov@Yuras-MacBook-Air.local>
2020-11-25 21:25:38 +0200

    Add address migration

--- TestWebApp/Migrations/20201125191745_AddressMigration.Designer.cs
+++ TestWebApp/Migrations/20201125191745_AddressMigration.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestWebApp.Data;
 
 namespace TestWebApp.Migrations
 {
     [DbContext(typeof(CustomerContext))]
-    partial class CustomerContextModelSnapshot : ModelSnapshot
+    [Migration("20201125191745_AddressMigration")]
+    partial class AddressMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
