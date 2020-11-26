0f52faaea67b400546c12f10faaace258ea077e9
Yura Vishnyakov <yuravishnyakov@Yuras-MacBook-Air.local>
2020-11-26 11:44:27 +0200

    a

---
+++ TestWebApp/Controllers/CustomersController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestWebApp.Data;
+using TestWebApp.Models;
+
+namespace TestWebApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly CustomerContext _context;
+
+        public CustomersController(CustomerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(FromSingleItem(customer));
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("CustomerId,FullName, Email, Birthsdate, Gender, Address")] Customer customer)
+        //{
+        //    if (id != customer.CustomerId)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(customer);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+                   
+        //                throw;
+                   
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(customer);
+        //}
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+
+        public string Index()
+        {
+            return "Hello Mtherfucker";
+        }
+
+
+
+        private static IEnumerable<T> FromSingleItem<T>(T item)
+        {
+            yield return item;
+        }
+    }
+}
