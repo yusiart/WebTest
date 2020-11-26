e7f042762ae73745c0d35ed2d99c164bc149ecf8
Yura Vishnyakov <yuravishnyakov@Yuras-MacBook-Air.local>
2020-11-26 16:24:44 +0200

    add view files

---
+++ TestWebApp/Controllers/AddressController.cs
@@ -0,0 +1,140 @@
+﻿//using System;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using TestWebApp.Models;
+
+//namespace TestWebApp.Controllers
+//{
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class AddressController : ControllerBase
+//    {
+//        public Address _address { get; }
+
+//        public AddressController(Address repo)
+//        {
+//            _address = repo;
+//        }
+
+//        [HttpPost("{customerId}")]
+//        public async Task<IActionResult> CreateAddress(int customerId, Address address)
+//        {
+//            try
+//            {
+//                address.CustomerId = customerId;
+//                address.CreatedAt = DateTime.Now;
+
+//                _address.Create(address);
+
+//                if (await _address.SaveChangesAsync())
+//                {
+//                    return Ok(address);
+//                }
+//            }
+//            catch (System.Exception ex)
+//            {
+//                return this.StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+//            }
+
+//            return BadRequest("Não foi possível salvar as informações");
+//        }
+
+//        [HttpPut]
+//        public async Task<IActionResult> UpdateAddress(Address address)
+//        {
+//            try
+//            {
+//                var addressAux = await _address.GetAddressByIdAsync(address.Id);
+
+//                if (addressAux == null)
+//                {
+//                    return NotFound();
+//                }
+//                else
+//                {
+//                    addressAux.Street = address.Street;
+//                    addressAux.Number = address.Number;
+//                    addressAux.ZipCode = address.ZipCode;
+//                    addressAux.Complement = address.Complement;
+//                    addressAux.City = address.City;
+//                    addressAux.State = address.State;
+//                    addressAux.MainAddress = address.MainAddress;
+//                    addressAux.UpdatedAt = DateTime.Now;
+
+//                    _address.Update(addressAux);
+
+//                    if (await _address.SaveChangesAsync())
+//                    {
+//                        return Ok(addressAux);
+//                    }
+//                }
+//            }
+//            catch (System.Exception ex)
+//            {
+//                return this.StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+//            }
+
+//            return BadRequest("Não foi possível salvar as informações");
+//        }
+
+//        [HttpDelete("{addressId}")]
+//        public async Task<IActionResult> DeleteAddress(int addressId)
+//        {
+//            try
+//            {
+//                var addressAux = await _address.GetAddressByIdAsync(addressId);
+
+
+//                if (addressAux == null)
+//                {
+//                    return NotFound();
+//                }
+//                else
+//                {
+//                    _address.Delete(addressAux);
+
+//                    if (await _address.SaveChangesAsync())
+//                    {
+//                        return Ok();
+//                    }
+//                }
+//            }
+//            catch (System.Exception ex)
+//            {
+//                return this.StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+//            }
+
+//            return BadRequest("Não foi possível excluir as informações");
+//        }
+
+//        [HttpGet("{addressId}")]
+//        public async Task<IActionResult> GetAddressByIdAsync(int addressId)
+//        {
+//            try
+//            {
+//                var address = await _address.GetAddressByIdAsync(addressId);
+
+//                return Ok(address);
+//            }
+//            catch (System.Exception ex)
+//            {
+//                return this.StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+//            }
+//        }
+
+//        [HttpGet("list/{customerId?}")]
+//        public async Task<IActionResult> ListAllAddressesAsync(int? customerId)
+//        {
+//            try
+//            {
+//                var addresses = await _address.GetAllAddressesAsync(customerId);
+
+//                return Ok(addresses);
+//            }
+//            catch (System.Exception ex)
+//            {
+//                return this.StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+//            }
+//        }
+//    }
+//}
--- TestWebApp/Controllers/CustomersController.cs
+++ TestWebApp/Controllers/CustomersController.cs
@@ -18,6 +18,13 @@ namespace TestWebApp.Controllers
             _context = context;
         }
 
+        // GET: Customer
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Customers.ToListAsync());
+        }
+
+        // GET: Customer/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -32,11 +39,31 @@ namespace TestWebApp.Controllers
                 return NotFound();
             }
 
-            return View(FromSingleItem(customer));
+            return View(customer);
         }
 
-     
+        // GET: Customer/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
+        // POST: Customer/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,FullName,Email,Birthdate,Gender,Address")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customer/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -52,6 +79,40 @@ namespace TestWebApp.Controllers
             return View(customer);
         }
 
+        // POST: Customer/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CustomerId,FullName,Email,Birthdate,Gender,Address")] Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customer/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -60,7 +121,7 @@ namespace TestWebApp.Controllers
             }
 
             var customer = await _context.Customers
-                .FirstOrDefaultAsync(c => c.CustomerId == id);
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
             if (customer == null)
             {
                 return NotFound();
@@ -69,16 +130,20 @@ namespace TestWebApp.Controllers
             return View(customer);
         }
 
-
-        public async Task<IActionResult> Index()
+        // POST: Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            return View(await _context.Customers.ToListAsync());
+            var customer = await _context.Customers.FindAsync(id);
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
-
-        private static IEnumerable<T> FromSingleItem<T>(T item)
+        private bool CustomerExists(int id)
         {
-            yield return item;
+            return _context.Customers.Any(e => e.CustomerId == id);
         }
     }
 }
